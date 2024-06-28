24c703adb3ff2fc39cea4a0838d4c3252f24a1de
Dawoodikram482 <mianmdawood482@gmail.com>
2024-06-28 19:19:40 +0200

    create user done

--- app/services/UserService.php
+++ app/services/UserService.php
@@ -51,6 +51,6 @@ class UserService
         $userDetails->role = role::admin;
 
         // Add user to repository
-        return $this->createNewUser($userDetails);
+        return $this->repository->addUser($userDetails);
     }
 }
